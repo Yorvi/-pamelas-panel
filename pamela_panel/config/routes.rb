aba9d8c088dd18d6d6c179b94e467775230c6bbc
Yorvi <jorvinrosario7@gmail.com>
2018-05-11 19:37:18 -0400

    added tables

--- pamela_panel/config/routes.rb
+++ pamela_panel/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :teacher_cohorts
+  resources :student_cohorts
+  resources :admins
+  resources :students
+  resources :teachers
+  resources :cohorts
+  resources :courses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
